f6d13e9e0765c700f8ac40d7accb685dadef50ba
Ciprian Salomir <ciprian.salomir@gmail.com>
2022-03-24 19:41:26 +0200

    Lesson 3 Update

---
+++ app/src/main/java/com/example/composetutorial/ui/theme/Style.kt
@@ -0,0 +1,9 @@
+package com.example.composetutorial.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.composetutorial.R
+
+val MyTextStyle = Typography.subtitle2.copy(
+    fontFamily = FontFamily(Font(R.font.audiowide)),
+)
--- app/src/main/java/com/example/composetutorial/ui/theme/Type.kt
+++ app/src/main/java/com/example/composetutorial/ui/theme/Type.kt
@@ -2,16 +2,15 @@ package com.example.composetutorial.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+//    defaultFontFamily = FontFamily(Font(R.font.akaya_telivigala)),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 18.sp
     )
     /* Other default text styles to override
     button = TextStyle(
